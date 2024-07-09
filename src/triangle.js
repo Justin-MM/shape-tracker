41f23c54b038bd0e0888cd7f8cb261a0249534e1
Justin Macharia Mwai <justinmacharia78@gmail>
2024-07-09 17:32:48 +0300

    fix linting issues

--- src/triangle.js
+++ src/triangle.js
@@ -5,11 +5,11 @@ export function Triangle(side1, side2, side3) {
 
     Triangle.prototype.isValidTriangle = function () {
         return !((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2)));
-    }
+    };
 
     Triangle.prototype.isScalene = function () {
         return this.isValidTriangle() && ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3)));
-    }
+    };
 
     Triangle.prototype.isIsosceles = function () {
         return this.isValidTriangle() &&
@@ -17,9 +17,9 @@ export function Triangle(side1, side2, side3) {
                 ((this.side1 === this.side2) && ((this.side1 !== this.side3))) ||
                 ((this.side1 !== this.side3) && ((this.side2 === this.side3)))
             );
-    }
+    };
 
     Triangle.prototype.isEquilateral = function () {
         return ((this.side1 === this.side2) && (this.side1 === this.side3));
-    }
+    };
 }
