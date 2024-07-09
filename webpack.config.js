43bd3aadc32573572587ea0d6da1e6e14b4863ef
Justin Macharia Mwai <justinmacharia78@gmail>
2024-07-09 15:49:41 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path =require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const{CleanWebpackPlugin}=require('clean-webpack-plugin');
+const ESLintPlugin=require('eslint-webpack-plugin');// new line!
+
+module.exports={
+  entry:'./src/index.js',
+  output:{
+    filename:'bundle.js',
+    path: path.resolve(__dirname,'dist'),
+  },
+  devtool:'eval-source-map',
+  devServer:{
+    static:{
+      directory: path.join(__dirname,"dist"),
+    },
+  },
+  plugins:[
+    new HtmlWebpackPlugin(),
+    new ESLintPlugin(),// new line!
+    new CleanWebpackPlugin(),
+  ]
+}
