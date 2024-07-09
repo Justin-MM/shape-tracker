e461c7c6fa6c21260c1eae582628fd0460e38388
Justin Macharia Mwai <justinmacharia78@gmail>
2024-07-09 17:10:58 +0300

    check if shape is a Valid Triangle

--- src/triangle.js
+++ src/triangle.js
@@ -1,5 +1,9 @@
-export default function Triangle(side1, side2, side3) {
+export function Triangle(side1, side2, side3) {
     this.side1 = side1;
     this.side2 = side2;
     this.side3 = side3;
+
+    Triangle.prototype.isValidTriangle = function() {
+        return !((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2)));
+    }
 }
