5b53e96fdb099ff8e92a4a0885a6bad43e98ba07
Justin Macharia Mwai <justinmacharia78@gmail>
2024-07-09 17:15:55 +0300

    check if triangle is scalene

--- src/triangle.js
+++ src/triangle.js
@@ -3,7 +3,11 @@ export function Triangle(side1, side2, side3) {
     this.side2 = side2;
     this.side3 = side3;
 
-    Triangle.prototype.isValidTriangle = function() {
+    Triangle.prototype.isValidTriangle = function () {
         return !((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2)));
+    }
+
+    Triangle.prototype.isScalene = function () {
+        return this.isValidTriangle() && ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3)));
     }
 }
